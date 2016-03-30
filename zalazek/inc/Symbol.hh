4ac53f12b5c1ba196e74d8ae418b29a1a01f235d
Pabu4444@wp.pl <user@localhost.localdomain>
2016-03-30 03:25:02 +0000

    debian v2

---
+++ zalazek/inc/Symbol.hh
@@ -0,0 +1,7 @@
+#ifndef SYMBOL_HH
+#define SYMBOL_HH
+
+enum class Symbol { e, a, b, c, d, blad };
+
+
+#endif
---
+++ zalazek/inc/WyrazenieAlgeb.hh
@@ -0,0 +1,18 @@
+#ifndef WYRAZENIEALGEB_HH
+#define WYRAZENIEALGEB_HH
+
+enum class Symbol;
+class Statystyka;
+
+struct WyrazenieAlgeb {
+
+  /*
+   *  Tutaj trzeba wstawic definicje odpowiednich pol
+   */
+	int Wprowadz(Symbol& arg1, Symbol& arg2);
+};
+
+
+
+
+#endif
